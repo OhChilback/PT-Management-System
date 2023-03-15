19b9cf81b5c710e5b93f5997c8f3ef45edd70d61
Ohchilback <wb3286@naver.com>
2023-03-15 18:31:31 +0900

    removed hello.java and added manumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add PT-Members");
+		System.out.println("2. Delete PT-Members");
+		System.out.println("3. Edit PT-Members");
+		System.out.println("4. View PT-Members");
+		System.out.println("5. Exit");
+	}
+
+}
